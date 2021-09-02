568555d94c71bf6c7f224ee4817102d69cb3e060
Sebastian Müller <sebastian@Sebastians-MacBook-Pro.local>
2021-09-02 16:57:12 +0200

    added hometile image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home/home_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: null,
+      home: HomeScreen(),
     );
   }
 }
