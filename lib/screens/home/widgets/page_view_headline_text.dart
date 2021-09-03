28feaee01636fc250fd79a6c99ff30cee560f170
Sebastian Müller <sebastian@Sebastians-MacBook-Pro.local>
2021-09-03 16:39:49 +0200

    added color change on pageview slide

--- lib/screens/home/widgets/page_view_headline_text.dart
+++ lib/screens/home/widgets/page_view_headline_text.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/home_screen_provider.dart';
+
 class PageViewHeadlineText extends StatelessWidget {
-  const PageViewHeadlineText({Key? key, this.headline}) : super(key: key);
+  const PageViewHeadlineText({Key? key, this.headline, this.index})
+      : super(key: key);
 
   final headline;
+  final index;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Text(
         headline,
-        style: TextStyle(),
+        style: GoogleFonts.roboto(
+          textStyle: TextStyle(
+            color: context.watch<HomeScreenProvider>().currentPageIndex == index
+                ? Colors.black
+                : Colors.green,
+          ),
+        ),
       ),
     );
   }
