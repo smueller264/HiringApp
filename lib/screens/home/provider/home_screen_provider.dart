f12b0e6bce6bc74102b2d9e708954631bfddc794
Sebastian Müller <sebastian@Sebastians-MacBook-Pro.local>
2021-09-03 16:05:21 +0200

    addd homescreen pageview and welcomepage + jobpage

--- lib/screens/home/provider/home_screen_provider.dart
+++ lib/screens/home/provider/home_screen_provider.dart
@@ -2,4 +2,26 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
-class HomeScreenProvider with ChangeNotifier {}
+class HomeScreenProvider with ChangeNotifier {
+  //PageController for HomeScreen PageView
+  final PageController pageController = PageController();
+
+  int _currentPageIndex = 0;
+  final int pageCount = 2;
+
+  ///Returns the currentPageIndex from the [PageController]
+  int get currentPageIndex => _currentPageIndex;
+
+  ///Sets new PageIndex and animates to new Page
+  set currentPageIndex(int newIndex) {
+    if (newIndex != _currentPageIndex) {
+      _currentPageIndex = newIndex;
+      notifyListeners();
+      pageController.animateToPage(
+        currentPageIndex,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+}
