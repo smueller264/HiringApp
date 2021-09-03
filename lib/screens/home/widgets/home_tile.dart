d8ccfe314fcd20a3dde68404259b01272d9d4501
Sebastian Müller <sebastian@Sebastians-MacBook-Pro.local>
2021-09-03 15:20:10 +0200

    updated tile width

--- lib/screens/home/widgets/home_tile.dart
+++ lib/screens/home/widgets/home_tile.dart
@@ -7,7 +7,7 @@ class HomeTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width * 1,
+      width: MediaQuery.of(context).size.width * 0.8,
       height: MediaQuery.of(context).size.height * 0.4,
       decoration: BoxDecoration(
         boxShadow: [
