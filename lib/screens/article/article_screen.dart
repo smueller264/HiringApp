375fe8481d5a1a62e8d398c716e590debd6794c1
Sebastian Müller <sebastian@Sebastians-MacBook-Pro.local>
2021-09-06 14:34:25 +0200

    added image header

--- lib/screens/article/article_screen.dart
+++ lib/screens/article/article_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../themes/device_size.dart';
+
 class ArticleScreen extends StatelessWidget {
   const ArticleScreen({Key? key}) : super(key: key);
 
@@ -10,13 +12,17 @@ class ArticleScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Container(
+              height: DeviceSize.height(context) * 0.35,
               decoration: BoxDecoration(
                 color: Color.fromRGBO(128, 102, 255, 1),
               ),
               child: Image.asset("assets/images/dxc1.png"),
             ),
+            Placeholder(),
           ],
         ),
       ),
