ba5c9ef040a991c0dd89306cbc9788becf23179f
Sebastian Müller <sebastian@Sebastians-MacBook-Pro.local>
2021-09-06 14:46:36 +0200

    finished articleScreen

--- lib/screens/article/article_screen.dart
+++ lib/screens/article/article_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../themes/device_size.dart';
+import '../../constants/strings.dart';
 
 class ArticleScreen extends StatelessWidget {
   const ArticleScreen({Key? key}) : super(key: key);
@@ -12,17 +13,65 @@ class ArticleScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Container(
+              margin: EdgeInsets.only(bottom: 20),
               height: DeviceSize.height(context) * 0.35,
               decoration: BoxDecoration(
                 color: Color.fromRGBO(128, 102, 255, 1),
               ),
               child: Image.asset("assets/images/dxc1.png"),
             ),
-            Placeholder(),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text("DISCOVER DXC"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, top: 2, bottom: 10),
+              child: Text(
+                "Was ist DXC",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 20, top: 2, bottom: 30, right: 10),
+              child: Text(
+                Strings.exampleText1(),
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 20, top: 2, bottom: 30, right: 10),
+              child: Text(
+                Strings.exampleText1(),
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 20, top: 2, bottom: 30, right: 10),
+              child: Text(
+                Strings.exampleText1(),
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
           ],
         ),
       ),
